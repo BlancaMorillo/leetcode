d25ea5daf532cec3301e96a6921bb474e2cf1e1d
Blanca Morillo <blancaemorillos@gmail.com>
2023-11-02 23:16:01 -0500

    Leetcode Problem 2235: Add two Integers (#3)

---
+++ algorithms/add_two_integers.py
@@ -0,0 +1,13 @@
+// Source : https://leetcode.com/problems/add-two-integers/description/
+// Author : Blanca Morillo
+// Date   : 2023-Nov-2
+
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        return num1 + num2
+        
